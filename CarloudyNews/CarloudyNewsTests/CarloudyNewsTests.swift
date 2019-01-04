6c85f62c17644453856425c26fdec8bbf49bbb3f
Zijia Zhai <zijiazhai@gmail.com>
2019-01-04 13:28:13 -0600

    init

---
+++ CarloudyNews/CarloudyNewsTests/CarloudyNewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  CarloudyNewsTests.swift
+//  CarloudyNewsTests
+//
+//  Created by Zijia Zhai on 1/4/19.
+//  Copyright © 2019 cognitiveAI. All rights reserved.
+//
+
+import XCTest
+@testable import CarloudyNews
+
+class CarloudyNewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
