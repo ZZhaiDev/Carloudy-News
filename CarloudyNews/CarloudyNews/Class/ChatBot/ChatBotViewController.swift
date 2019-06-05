13da1b67031907c72d8af8d86c44e2edaaa4c9cd
Zijia Zhai <zijiazhai@gmail.com>
2019-06-05 14:02:35 -0500

    new branch test

--- CarloudyNews/CarloudyNews/Class/ChatBot/ChatBotViewController.swift
+++ CarloudyNews/CarloudyNews/Class/ChatBot/ChatBotViewController.swift
@@ -58,7 +58,7 @@ class ChatBotViewController: UIViewController {
         speechSynthesizer.speak(speechUtterance)
         sendTextF.text = ""
         resultL.text = text
-        ZJPrint("")
+        ZJPrint("New Branch Test")
     }
 
 }
